a379bc84549e50c25779fa3b52a2c45f4850ed67
reisir <reisirito@gmail.com>
2023-04-01 01:31:09 +0300

    fixed shit

--- room.js
+++ room.js
@@ -1,100 +1,133 @@
-var scale = 1.6;
-var roomDimensions = [308, 442];
-var objects = [];
-var currentId;
-var i;
-var bruh = [];
+const scale = 2;
+const dimensions = [308, 442];
+
+let room = [];
+let currentId;
+let snap = true;
 
 $(document).ready(() => {
-  $("#room")
-    .width(roomDimensions[0] * scale)
-    .height(roomDimensions[1] * scale);
+  room = JSON.parse(localStorage.getItem("room")) || [];
+  render();
 });
 
-$(document).keypress(function (keyEvent) {
+$(document).on("keypress", function (keyEvent) {
+  console.log(keyEvent.which);
   switch (keyEvent.which) {
+    // (D)elete
+    case 100: {
+      room = room
+        .filter((i) => i.id != currentId)
+        .map((item, index) => ({ ...item, id: index }));
+      save();
+      render();
+      break;
+    }
+    // (R)otate
     case 114: {
-      var currentW = $(currentId).width();
-      $(currentId).width($(currentId).height()).height(currentW);
+      rotate();
+      render();
+      break;
     }
+    // (S)napping
     case 115: {
-      if ($(currentId).draggable("option", "snap") == true) {
-        $(currentId).draggable("option", "snap", false);
-      } else {
-        $(currentId).draggable("option", "snap", true);
-      }
+      snap = !snap;
+      render();
+      break;
     }
-    case 116: {
-      var object = {
+    // (N)ew
+    case 110: {
+      const item = {
+        id: room.length,
         name: promptUser("name", "object"),
-        objH: promptUser("height", "100"),
-        objW: promptUser("width", "100"),
+        h: promptUser("height", "100") * 2,
+        w: promptUser("width", "100") * 2,
         color: promptUser("color in hex", "#FFFFFF"),
+        top: 0,
+        left: 0,
       };
-      objects.push(object);
-      i++;
-      $("#room").append(newElement(object, i));
-      makeAllDraggable();
+      room.push(item);
+      render();
+      break;
     }
+    // save to (C)ookie
     case 99: {
-      Cookies.set("roomArr", JSON.stringify(objects), {
-        expires: 100,
-      });
+      save();
+      break;
     }
+    // (L)oad from cookie
     case 108: {
-      //Load from cookie
-      console.log(Cookies.get("roomArr"));
-      loadRoomArray(JSON.parse(Cookies.get("roomArr")));
+      load();
+      break;
     }
   }
 });
 
+function save() {
+  localStorage.setItem("room", JSON.stringify(room));
+}
+
+function load() {
+  room = JSON.parse(localStorage.getItem("room"));
+  render();
+}
+
 function promptUser(property, defaultVal) {
-  var getValue = prompt("Please enter object " + property, defaultVal);
-  return getValue;
+  return prompt(`Please enter item ${property}`, defaultVal);
 }
 
-function newElement(object, i) {
-  var html =
-    '<div class="object" id="object' +
-    i +
-    '" style=" width:' +
-    object.objW * scale +
-    "px; height:" +
-    object.objH * scale +
-    "px; background: " +
-    object.color +
-    '"><h1>' +
-    object.name +
-    "</h1></div>";
-  return html;
+function rotate() {
+  if (!room[currentId]) return;
+  const { w, h } = room[currentId];
+  room[currentId].h = w;
+  room[currentId].w = h;
+  render();
 }
 
-function makeAllDraggable() {
-  $(".object").draggable({
-    snap: true,
+function render() {
+  console.log("render", { snap });
+
+  $room = $("#room");
+  $room.empty();
+
+  $room.width(dimensions[0] * scale).height(dimensions[1] * scale);
+
+  for (const item of room) {
+    $("<div/>", {
+      class: `item`,
+      id: item.id,
+    })
+      .css({
+        width: item.w,
+        height: item.h,
+        backgroundColor: item.color,
+        left: item.left,
+        top: item.top,
+      })
+      .append(`<h1>${item.name}</h1>`)
+      .appendTo($room);
+  }
+
+  // Make all draggable
+  $(".item").draggable({
+    snap,
     containment: "parent",
     cursor: "pointer",
     start: function (event, ui) {
-      currentId = "#" + $(this).attr("id");
-      console.log(currentId);
+      currentId = event.target.id;
+      select();
+    },
+    stop: function (event, ui) {
+      const id = this.id;
+      room[id].left = this.style.left;
+      room[id].top = this.style.top;
+      save();
     },
   });
+
+  select();
 }
 
-function loadRoomArray(bruh) {
-  bruh.forEach(function (object) {
-    objects.push(object);
-  });
-
-  objects.forEach(function (object, i) {
-    $("#room").append(newElement(object, i));
-    i++;
-  });
-
-  makeAllDraggable();
-
-  $(".object").click(function () {
-    currentId = "#" + $(this).attr("id");
-  });
+function select() {
+  $(".item").removeClass("selected");
+  $(`#${currentId}`).addClass("selected");
 }
